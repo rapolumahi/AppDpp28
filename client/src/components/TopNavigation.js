de1d990960f02a0dd98e12e2fbb7ea40fd96aebd
rapolumahi <mahi.vbit@gmail.com>
2024-01-28 11:37:03 -0600

    added files

---
+++ client/src/components/TopNavigation.js
@@ -0,0 +1,66 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+function TopNavigation() {
+  let storeObj = useSelector((store) => {
+    return store;
+  });
+  console.log(storeObj);
+  let navigate = useNavigate();
+  useEffect(() => {
+    if (
+      storeObj &&
+      storeObj.userData &&
+      storeObj.userData.data &&
+      storeObj.userData.data.firstName
+    ) {
+    } else {
+      navigate("/");
+    }
+  }, []);
+  let deleteAccount = async () => {
+    let dataToSend = new FormData();
+    dataToSend.append("gmail", storeObj.userData.data.gmail);
+    let response = await axios.delete(
+      "http://localhost:6565/deleteUser",
+      dataToSend
+    );
+    console.log(response);
+    if (response.data.status == "success") {
+      alert(response.data.msg);
+      navigate("/");
+    } else {
+      alert(response.data.msg);
+    }
+  };
+  return (
+    <div>
+      <button>HOME</button>
+      <button
+        onClick={() => {
+          navigate("/editprofile");
+        }}
+      >
+        EDITPROFILE{" "}
+      </button>
+      <button
+        onClick={() => {
+          deleteAccount();
+        }}
+      >
+        DELETE
+      </button>
+      <button
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        LOGOUT
+      </button>
+    </div>
+  );
+}
+
+export default TopNavigation;
